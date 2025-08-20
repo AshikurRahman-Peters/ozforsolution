cf7ffe1a44d08a778d09c9132d19a57bf87d87fa
AshikurRahman-Peters <ashikurrahmanpeters@gmail.com>
2025-08-20 12:57:13 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+  function switchLang(lang) {
+    if (lang === 'en') {
+      window.location.href = "index-en.html"; // your English page
+    } else {
+      window.location.href = "index.html"; // your Arabic page
+    }
+  }
